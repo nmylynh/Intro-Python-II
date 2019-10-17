cfba27e87ebac3f43cd709d3aa7d1adadf40b5a3
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-10-16 20:33:02 -0700

    Update room.py

--- src/room.py
+++ src/room.py
@@ -1,2 +1,25 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description, items=None, n_to=None, s_to=None, e_to=None, w_to=None):
+        self.name = name
+        self.description = description
+        if items is None:
+            self.items= {}
+        else:
+            self.items = items
+        self.n_to = n_to
+        self.s_to = s_to
+        self.e_to = e_to
+        self.w_to = w_to
+    
+    def __str__(self):
+        return {'name': self.name, 'description': self.description, 'items': self.items}
+
+
+        Room('Kitchen', 'You make food here', Item('candle', 'it lights up shit'))
+
+
+class Item:
+    def
