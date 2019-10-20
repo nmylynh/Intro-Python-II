8175a9f0571ec0f0e9a7e7c7f853df60395dd8d5
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-10-19 22:23:16 -0700

    Update room.py

--- src/room.py
+++ src/room.py
@@ -21,9 +21,16 @@ class Room:
         if len(self.items) == 0:
             print('There are no items here.')
         else:
-            check = input(f'There seems to be a few item(s) lying around in the {self.name}. Would you like to check them? /n Yes(y), No(n)')
+            check = input(f'There seems to be a few item(s) lying around in the {self.name}. Would you like to check them? \n Yes(y), No(n)')
             if check == 'y':
-                for item, description in self.items:
-                    print(f'You found a(n) {item}: {description}.') 
+                for item, description in self.items.items():
+                    print(f'{item}: {description}') 
             else: 
                 print('You ignore the items in the room.')
+
+
+dungeon = Room('Dungeon', 'Damp, cobblestoned walls surround you as you enter, within the depths of this structure you see metal cages and shackles. You hear eerie groans in the distance.', items={'keys': 'A ring of keys you found on a hook attached to the ebony walls of the dungeon.', 'whip': 'A leather whip you found on a metal desk of the dungeon.'})
+
+standard_input = 'y'
+
+print(dungeon.room_items())
